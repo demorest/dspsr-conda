3e2eaf111952c151d600f6a710a33aee632ac7ed
Paul Demorest <pdemores@nrao.edu>
2019-10-21 19:21:14 -0600

    Strip -std=c++XX from CXXFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 ./bootstrap
+# Strip any c++ standard specs, configure will add c++11
+export CXXFLAGS=$(echo "$CXXFLAGS" | perl -pe 's/-std=c.+ / /')
+echo "CXXFLAGS $CXXFLAGS"
 echo "asp mark5 guppi dummy vdif sigproc fits lwa" > backends.list
 ./configure --prefix=$PREFIX --disable-local --enable-shared \
   --with-Qt-dir=no \
